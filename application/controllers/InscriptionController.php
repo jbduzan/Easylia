586dd4c97cdcdb2d98f679adfdbecda24b7cc6d0
Jean-Baptiste Duzan <jbduzan@gmail.com>
2012-02-03 10:18:25 +0100

    correction bug envoie mail inscription + ajout lien renvoie mail confirmation

--- application/controllers/InscriptionController.php
+++ application/controllers/InscriptionController.php
@@ -49,7 +49,7 @@ class InscriptionController extends Zend_Controller_Action
                 $mail->setFrom('no-reply@easylia.com', 'Easylia');
                 $mail->addTo($client->getMail());
                 $mail->setSubject("Bienvenue chez Easylia !");
-                $mail->setBodyHtml(utf8_decode("<div><img src='http://in.easylia.com/images/logo.jpg'/></div><div><p>Bonjour, <br /><br />Vous venez de vous préinscrire comme formateur chez Easylia, et nous vous en remercions. <br /><br /> Nous vous invitons tout d'abord à cliquer sur le lien suivant pour terminer votre préinscription : <br /><a href='http://in.easylia.com/activation-compte?id=".$id."&key=".$client->getCleActivation()."'>Activer votre compte</a><br /><br />Vous pourrez ensuite vous connecter sur votre <a href='http://in.easylia.com/profil-utilisateur'>espace personnel</a>, afin de poursuivre la procédure d'inscription.<br /><br />Une fois celle-ci achevée, vous pourrez accéder à la liste des formations disponibles et à donner.<br /><br />En cas de questions, nous vous invitons à consulter la FAQ, accessible depuis notre site Internet. <br /><br />Restant à votre écoute,<br />Nous vous souhaitons la bienvenue chez Easylia.<br /><br />Cordialement, <br />L'équipe Easylia. </p></div>"));
+                $mail->setBodyHtml(utf8_decode("<div><img src='https://in.easylia.com/images/logo.jpg'/></div><div><p>Bonjour, <br /><br />Vous venez de vous inscrire comme formateur chez Easylia, et nous vous en remercions. <br /><br /> Nous vous invitons tout d'abord à cliquer sur le lien suivant pour terminer votre préinscription : <br /><a href='http://in.easylia.com/activation-compte?id=".$id."&key=".$client->getCleActivation()."'>Activer votre compte</a><br /><br />Vous pourrez ensuite vous connecter sur votre <a href='http://in.easylia.com/profil-utilisateur'>espace personnel</a>, afin de poursuivre la procédure d'inscription.<br /><br />Une fois celle-ci achevée, vous pourrez accéder à la liste des formations disponibles et à donner.<br /><br />En cas de questions, nous vous invitons à consulter la FAQ, accessible depuis notre site Internet. <br /><br />Restant à votre écoute,<br />Nous vous souhaitons la bienvenue chez Easylia.<br /><br />Cordialement, <br />L'équipe Easylia. </p></div>"));
                 $mail->send();
 
                                                              
@@ -64,7 +64,24 @@ class InscriptionController extends Zend_Controller_Action
 		$this->userMapper->find($this->getRequest()->getParam('id'), $utilisateur);
 		
 		$this->view->email = $utilisateur->getMail();
+        $this->view->id_utilisateur = $utilisateur->getIdUtilisateur();
 	}
+
+    public function renvoiemailactivationAction(){
+        // Renvoie le mail d'activation
+        $this->_helper->viewRenderer->setNoRender(true);
+
+        $id = $this->getRequest()->getParam('id');
+        $client = new Application_Model_Utilisateurs();
+        $this->userMapper->find($id, $client);
+
+        $mail = new Zend_Mail();
+        $mail->setFrom('no-reply@easylia.com', 'Easylia');
+        $mail->addTo($client->getMail());
+        $mail->setSubject("Bienvenue chez Easylia !");
+        $mail->setBodyHtml(utf8_decode("<div><img src='https://in.easylia.com/images/logo.jpg'/></div><div><p>Bonjour, <br /><br />Vous venez de vous inscrire comme formateur chez Easylia, et nous vous en remercions. <br /><br /> Nous vous invitons tout d'abord à cliquer sur le lien suivant pour terminer votre préinscription : <br /><a href='http://in.easylia.com/activation-compte?id=".$id."&key=".$client->getCleActivation()."'>Activer votre compte</a><br /><br />Vous pourrez ensuite vous connecter sur votre <a href='http://in.easylia.com/profil-utilisateur'>espace personnel</a>, afin de poursuivre la procédure d'inscription.<br /><br />Une fois celle-ci achevée, vous pourrez accéder à la liste des formations disponibles et à donner.<br /><br />En cas de questions, nous vous invitons à consulter la FAQ, accessible depuis notre site Internet. <br /><br />Restant à votre écoute,<br />Nous vous souhaitons la bienvenue chez Easylia.<br /><br />Cordialement, <br />L'équipe Easylia. </p></div>"));
+        $mail->send();
+    }
 
 }
 
